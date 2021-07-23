8d590e595892745574c25003631adc5c778dc223
JS <preketchup@gmail.com>
2021-07-24 00:48:29 +1200

    AUT Link Added

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="AUT-link"
+          className="App-link"
           href="https://aut.ac.nz"
           target="_blank"
           rel="noopener noreferrer"

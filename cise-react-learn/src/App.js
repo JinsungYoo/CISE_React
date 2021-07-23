c153ce5df5c6b1d45b47a93802813100da630d7e
JS <preketchup@gmail.com>
2021-07-24 00:46:10 +1200

    ClassName Changed

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="AUT-link"
           href="https://aut.ac.nz"
           target="_blank"
           rel="noopener noreferrer"
